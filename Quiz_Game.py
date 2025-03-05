ff1ff5788fba9edf2a2be0fdbb7213f1667b0365
Jai Singh Rajawat <jaisinghrajawatt@gmail.com>
2025-03-05 21:38:16 +0530

    added code

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -10,7 +10,40 @@ options = (("A. 116", "B. 117", "C. 118", "D. 119"),
            ("A. 206", "B. 207", "C. 208", "D. 209"),
            ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
 
-aneswers = ("C", "D", "A", "A", "B")
+answers = ("C", "D", "A", "A", "B")
 guesses = []
 score = 0
 question_num = 0
+
+for question in questions:
+    print("----------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+        print(f"{answers[question_num]} is the correct anewer")
+    question_num += 1
+
+print("----------------------------")
+print("           RESULTS          ")
+print("----------------------------")
+
+print("Answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("Guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+score = int(score/ len(questions) * 100)
+print(f"Your Score is: {score}%")
