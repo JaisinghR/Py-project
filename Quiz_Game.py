f48c8e5b26eaecd6a9c40aafaedf55230fd3db5d
Jai Singh Rajawat <jaisinghrajawatt@gmail.com>
2025-03-05 20:12:43 +0530

    adding Quiz_Game.py

---
+++ Quiz_Game.py
@@ -0,0 +1,16 @@
+questions = ("How many elements are in the periodic table?: ",
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in the Human body?: ",
+             "Which planet in the Solar System is the Hottest?: ")
+
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
+           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dixoide", "D. Hydrogen"),
+           ("A. 206", "B. 207", "C. 208", "D. 209"),
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+
+aneswers = ("C", "D", "A", "A", "B")
+guesses = []
+score = 0
+question_num = 0
